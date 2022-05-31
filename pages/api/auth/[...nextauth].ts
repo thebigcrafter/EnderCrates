c32881eb11191cd5aaad03b4272c5d228ea77012
MintoD3v <tobydev27@outlook.com>
2022-06-01 00:02:05 +0700

    Always use GitHub username

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 
 import client from "../../../db/client";
 
@@ -9,7 +9,15 @@ export default NextAuth({
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
     }),
   ],
-  adapter: MongoDBAdapter(client)
+  adapter: MongoDBAdapter(client),
 });
